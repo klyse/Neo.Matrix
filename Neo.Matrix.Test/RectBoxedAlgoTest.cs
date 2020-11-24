f7fe6ce916d28f3b2a6d78a2b3f27936b0f84a41
Klyse <Klaus.Pruenster@gmail.com>
2020-11-24 20:28:36 +0100

    added stride to RectBoxedAlgo

---
+++ Neo.Matrix.Test/RectBoxedAlgoTest.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using NeoMatrix.Exceptions;
+using NUnit.Framework;
+
+namespace NeoMatrix.Test
+{
+	[TestFixture]
+	public class RectBoxedAlgoTest
+	{
+		[SetUp]
+		public void SetUp()
+		{
+			var val = 0;
+			Matrix = Matrix<DummyObject>.NewMatrix(4, 4, () =>
+			{
+				val++;
+				return new DummyObject
+				{
+					Value = val
+				};
+			});
+		}
+
+		public class DummyObject
+		{
+			public int Value { get; set; }
+		}
+
+		public Matrix<DummyObject> Matrix { get; set; }
+
+		[Test]
+		public void RectBoxedAlgo_CorrectSize()
+		{
+			var sum = Matrix.RectBoxedAlgo(3, 3, (_, _, m) => m.Sum(c => c.Value));
+
+			Assert.AreEqual(2, sum.Rows);
+			Assert.AreEqual(2, sum.Columns);
+		}
+
+		[Test]
+		[TestCase(-1, -1)]
+		[TestCase(-3, 3)]
+		[TestCase(5, 1)]
+		[TestCase(1, 5)]
+		[TestCase(5, 5)]
+		public void RectBoxedAlgo_NotAllowedDimension(int rows, int columns)
+		{
+			Assert.Throws<OutOfRangeException>(() => Matrix.RectBoxedAlgo(rows, columns, (_, _, m) => m.Sum(c => c.Value)));
+		}
+
+		[Test]
+		public void RectBoxedAlgo_ReturnSum()
+		{
+			var expected = new Matrix<double>(new double[,]
+			{
+				{54, 63},
+				{90, 99}
+			});
+
+			var sum = Matrix.RectBoxedAlgo(3, 3, (_, _, m) => m.Sum(c => c.Value));
+
+			Assert.AreEqual(expected, sum);
+		}
+
+		[Test]
+		public void RectBoxedAlgo_CalculateBigArray()
+		{
+			var matrix = Matrix<DummyObject>.NewMatrix(300, 300, () => new DummyObject
+			{
+				Value = new Random().Next(0, 3000)
+			});
+
+			var filtered = matrix.RectBoxedAlgo(11, 11, (_, _, m) => m.Average(c => c.Value));
+
+			// matrix.ToBitmap(c => c.Value).Save("C:\\Users\\Klaus\\Downloads\\test.bmp");
+			// filtered.ToBitmap(c => c).Save("C:\\Users\\Klaus\\Downloads\\test1.bmp");
+
+			Assert.AreEqual(290, filtered.Columns);
+			Assert.AreEqual(290, filtered.Rows);
+		}
+
+		[Test]
+		public void RectBoxedAlgo_CalculateBigArray_Stride2()
+		{
+			var matrix = Matrix<DummyObject>.NewMatrix(300, 300, () => new DummyObject
+			{
+				Value = new Random().Next(0, 3000)
+			});
+
+			var filtered = matrix.RectBoxedAlgo(11, 11, (_, _, m) => m.Average(c => c.Value), 2, 2);
+
+			// matrix.ToBitmap(c => c.Value).Save("C:\\Users\\Klaus\\Downloads\\test.bmp");
+			// filtered.ToBitmap(c => c).Save("C:\\Users\\Klaus\\Downloads\\test1.bmp");
+
+			Assert.AreEqual(145, filtered.Columns);
+			Assert.AreEqual(145, filtered.Rows);
+		}
+
+		[Test]
+		public void RectBoxedAlgo_CalculateBigArray_YStride5_XStride10()
+		{
+			var matrix = Matrix<DummyObject>.NewMatrix(300, 600, () => new DummyObject
+			{
+				Value = new Random().Next(0, 3000)
+			});
+
+			var filtered = matrix.RectBoxedAlgo(11, 11, (_, _, m) => m.Average(c => c.Value), 5, 10);
+
+			matrix.ToBitmap(c => c.Value).Save("C:\\Users\\Klaus\\Downloads\\test.bmp");
+			filtered.ToBitmap(c => c).Save("C:\\Users\\Klaus\\Downloads\\test1.bmp");
+
+			Assert.AreEqual(59, filtered.Columns);
+			Assert.AreEqual(58, filtered.Rows);
+		}
+
+		[Test]
+		[TestCase(3, 5)]
+		[TestCase(5, 3)]
+		public void RectBoxedAlgo_StrideOutOfRange_Exception(int yStride, int xStride)
+		{
+			var matrix = Matrix<DummyObject>.NewMatrix(300, 600, () => new DummyObject
+			{
+				Value = new Random().Next(0, 3000)
+			});
+
+			Assert.Throws<MatrixException>(() => matrix.RectBoxedAlgo(11, 11, (_, _, m) => m.Average(c => c.Value), yStride, xStride));
+		}
+
+		[Test]
+		[TestCase(0, 0)]
+		[TestCase(0, 1)]
+		[TestCase(1, 0)]
+		public void RectBoxedAlgo_InvalidStride_Exception(int yStride, int xStride)
+		{
+			var matrix = Matrix<DummyObject>.NewMatrix(300, 600, () => new DummyObject
+			{
+				Value = new Random().Next(0, 3000)
+			});
+
+			Assert.Throws<OutOfRangeException>(() => matrix.RectBoxedAlgo(11, 11, (_, _, m) => m.Average(c => c.Value), yStride, xStride));
+		}
+
+		[Test]
+		[TestCase(0, 1)]
+		[TestCase(4, 3)]
+		public void RectBoxedAlgo_EvenRows_ThrowsErrors(int rows, int columns)
+		{
+			Assert.Throws<EvenRowsException>(() => Matrix.RectBoxedAlgo(rows, columns, (_, _, m) => m.Sum(c => c.Value)));
+		}
+
+		[Test]
+		[TestCase(1, 0)]
+		[TestCase(5, 4)]
+		public void RectBoxedAlgo_EvenColumns_ThrowsErrors(int rows, int columns)
+		{
+			Assert.Throws<EvenColumnsException>(() => Matrix.RectBoxedAlgo(rows, columns, (_, _, m) => m.Sum(c => c.Value)));
+		}
+	}
+}
--- Neo.Matrix/MatrixFilter.cs
+++ Neo.Matrix/MatrixFilter.cs
@@ -15,8 +15,10 @@ namespace NeoMatrix
 		/// <param name="rows">rows</param>
 		/// <param name="columns">columns</param>
 		/// <param name="func">function for filter</param>
+		/// <param name="yStride">steps in y direction</param>
+		/// <param name="xStride">steps in x direction</param>
 		/// <returns>matrix with results</returns>
-		public static Matrix<double> RectBoxedSum<TType>(this Matrix<TType> matrix, int rows, int columns, Func<Matrix<TType>, double> func)
+		public static Matrix<double> RectBoxedAlgo<TType>(this Matrix<TType> matrix, int rows, int columns, Func<int, int, Matrix<TType>, double> func, int yStride = 1, int xStride = 1)
 		{
 			EvenRowsException.Check(rows);
 			EvenColumnsException.Check(columns);
@@ -24,18 +26,32 @@ namespace NeoMatrix
 			OutOfRangeException.Check(columns, 0, matrix.Columns);
 			OutOfRangeException.Check(rows, 0, matrix.Rows);
 
-			var colOffset = (columns - 1) / 2;
-			var rowOffset = (rows - 1) / 2;
+			OutOfRangeException.Check(xStride, 0);
+			OutOfRangeException.Check(yStride, 0);
 
-			var resCols = matrix.Columns - 2 * colOffset;
+			var rowOffset = (rows - 1) / 2;
+			var colOffset = (columns - 1) / 2;
+
 			var resRows = matrix.Rows - 2 * rowOffset;
+			var resCols = matrix.Columns - 2 * colOffset;
+
+			if (resRows % yStride != 0)
+				throw new MatrixException("rows must be divisible by stride");
+
+			if (resCols % xStride != 0)
+				throw new MatrixException("columns must be divisible by stride");
+
+			resRows /= yStride;
+			resCols /= xStride;
 
 			var returnMat = new Matrix<double>(resRows, resCols);
 
 			// k.p. todo: save already calculated values to speedup algorithm
-			for (var i = rowOffset; i < matrix.Rows - rowOffset; i++)
-			for (var j = colOffset; j < matrix.Columns - colOffset; j++)
-				returnMat[i - rowOffset, j - colOffset] = func(matrix.GetRect(i, j, columns, rows));
+			var column = 0;
+			var row = 0;
+			for (var i = rowOffset; i < matrix.Rows - rowOffset; i += yStride, row++, column = 0)
+			for (var j = colOffset; j < matrix.Columns - colOffset; j += xStride, column++)
+				returnMat[row, column] = func(i, j, matrix.GetRect(i, j, columns, rows));
 
 			return returnMat;
 		}
